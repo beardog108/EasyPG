f8170a77c597d70f63ae17a56045c08e26946e46
Duncan X. Simpson <virtualdxs@gmail.com>
2016-08-21 18:41:34 -0700

    Set up keygen and worked on flow

--- main.sh
+++ main.sh
@@ -1,18 +1,62 @@
 #!/bin/bash
 
-# Store menu options selected by the user
-INPUT=/tmp/menu.sh.$$
 export LD_PRELOAD=$(dirname $0)/randpatch/randpatch.so
+export USER=$(whoami) # Just in case it's not already set
+
+new_user() {
+    whiptail --msgbox "Looks like you haven't used GPG before. Welcome! Let's get you set up." 7 80
+    gen_key
+}
+
+gen_key() {
+    GPGSCRIPT=$(mktemp)
+    echo -e "Key-Type: RSA\nSubkey-Type: RSA\nKey-Length: 4096\nSubkey-Length: 4096" > $GPGSCRIPT
+    REALNAME=$(whiptail --inputbox "Please enter your full name." 10 60 ${USER^} 3>&1 1>&2 2>&3)
+    EMAIL=$(whiptail --inputbox "Please enter your email address." 10 60 $USER@ 3>&1 1>&2 2>&3)
+    COMMENT=$(whiptail --inputbox "Enter an optional comment for your key. For example, it could be your website or Keybase profile." 10 60 3>&1 1>&2 2>&3)
+    echo -e "Name-Real: $REALNAME\nName-Email: $EMAIL" >> $GPGSCRIPT
+    if [ "$COMMENT" != "" ];then echo Name-Comment: $COMMENT >> $GPGSCRIPT;fi
+    echo -e "Expire-Date: 0" >> $GPGSCRIPT
+    while true; do
+        PASSPHRASE=$(whiptail --passwordbox "Please enter a password for your key." 10 60 3>&1 1>&2 2>&3)
+        if [ "$PASSPHRASE" == "" ]; then whiptail --msgbox "Sorry, password can't be blank." 10 60;
+            else PASSCHK=$(whiptail --passwordbox "Please reenter the password for your key." 10 60 3>&1 1>&2 2>&3)
+            if [ "$PASSPHRASE" != "$PASSCHK" ]; then whiptail --msgbox "Sorry, passwords don't match." 10 60
+                else break
+            fi
+        fi
+    done
+    echo "Passphrase: $PASSPHRASE" >> $GPGSCRIPT
+    echo "%commit" >> $GPGSCRIPT
+    if whiptail --yesno "Do you want to generate this key?\nName: $REALNAME\nEmail: $EMAIL\nComment: $COMMENT" 10 40;then gpg --batch --gen-key $GPGSCRIPT;fi
+    rm -f $GPGSCRIPT
+    sleep 1
+}
 
 # Main menu
-while [ 1 ]
-do
-
-COLOR=$(whiptail --menu --nocancel --noitem "EasyPG - Main Menu" 15 20 5 "Generate Key" "" "List Keys" "" "Encryption" "" "Signing" "" 3>&1 1>&2 2>&3)
-
+main_menu() {
+SELECTION=$(whiptail --menu --nocancel --notags "EasyPG - Main Menu" 15 30 5 "gen" "Generate Key" "list" "List Keys" "enc" "Encryption" "sign" "Signing" "exit" "Exit" 3>&1 1>&2 2>&3)
+echo $SELECTION
+case $SELECTION in
+    "gen") 
+        gen_key
+	;;
+    "list")
+        echo TODO
+	;;
+    "enc")
+        echo TODO
+	;;
+    "sign")
+        echo TODO
+	;;
+    "exit")
+        exit
+	;;
+esac
 sleep 1
-echo $COLOR
-sleep 2
-done
+}
 
-rm $INPUT
+if [ ! -d ~/.gnupg ]; then new_user;fi
+
+while true; do main_menu; done
